f9f30fffc0d0b70b6dc4fffd199bc43be77cba9a
Craig Wellspring <wellspring.craig@gmail.com>
2021-10-23 10:58:06 -0500

    theming

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const HomeImage = styled.img`
+  max-width: 500px;
+`;
 
 export default function Home({ user }) {
   return (
     <>
       <h1>Welcome, {user.fullName}!</h1>
+      <HomeImage
+        src="https://www.wallpapertip.com/wmimgs/228-2285371_usa-soccer-wallpaper.jpg"
+        alt="Team Logo"
+      />
     </>
   );
 }
